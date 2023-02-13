a7a84d7b13e0ab1f24775a7442b2282f4c62677b
akmot <avicocyprien@yahoo.com>
2023-02-13 13:31:25 +0100

    v3

--- README.md
+++ README.md
@@ -10,9 +10,51 @@ git clone https://github.com/yourusername/csv-encoding-fixer.git
 ## Usage
 1. Place the CSV file you want to fix in the same directory as the script.
 2. Open a command prompt or terminal and navigate to the script directory.
-3. Run the script using the command python csv_encoding_fixer.py input_file.csv output_file.csv
+3. Run the script using the command :
+```Bash
+python csv_encoding_fixer.py input_file.csv output_file.csv
+```
 
-Replace input_file.csv with the name of the CSV file you want to fix, and replace output_file.csv with the name you want to give to the fixed CSV file.
+Replace `input_file.csv` with the name of the CSV file you want to fix, and replace `output_file.csv` with the name you want to give to the fixed CSV file.
+
+## Encoding Errors
+This script currently replaces the following encoding errors:
+
+| Errors | replacer |
+| ------ |----------|
+| Ã® | î        |
+| Ã© | 	é       |
+â†’| 	-->     |
+Ãœ	| Ü        |
+Ã¹	| û        |
+Ã¢	| â        |
+Ã¨	| è        |
+Ã | 	à       |
+â€™| 	'       |
+â‚¬| 	€       |
+Â®| 	®       |
+Ã˜	| Ø        |
+Â°	| °        |
+Ã§| 	ç       |
+Â®|	®
+Ã˜|	Ø
+Â°|	°
+Ã§|	ç
+Ã´|	ô
+Â«|	"
+Â»|	"
+Ã»|	û
+Ãª|	ê
+â€¦|	ø
+/Ã¸	|ø
+Ã€|	A
+Ã‰|	E
+Ãˆ|	E
+Ã|	à
+Ã–|	Ö
+
+## License
+This script is licensed under the <u>MIT License</u>. Feel free to modify and distribute it as needed.
 
 ## Troubleshooting
 If you encounter any issues with the script, please create an issue on the GitHub repository with a detailed description of the problem and any error messages you received. We will do our best to assist you in resolving the issue.
--- src.py
+++ src.py
@@ -1,27 +1,22 @@
+import sys
 import csv
 
-# Define a function to fix encoding errors
 def fix_encoding(text):
-    # List of characters to replace and their replacements
-    to_replace = ['Ã®', 'Ã©', 'â†’', 'Ãœ', 'Ã¹', 'Ã¢', 'Ã¨', 'Ã ', 'â€™', 'â‚¬', 'Â®', 'Ã˜', 'Â°', 'Ã§', 'Ã´', 'Â«', 'Â»', 'Ã»', 'Ãª', 'â€¦', '/Ã¸', 'Ã¸', 'Ã€', 'Ã‰', 'Ãˈ', ' Ã ', 'Ã–']
-    replacements = ['î', 'é', '-->', 'Ü', 'û', 'â', 'è', 'à', "'", '€', '®', 'Ø', '°', 'ç', 'ô', '«', '»', 'û', 'ê', '..', 'ø', 'ø', 'A', 'E', 'E', ' à ', 'Ö']
+    return text.replace('Ã®', 'î').replace('Ã©', 'é').replace('â†’', '-->').replace('Ãœ', 'Ü').replace('Ã¹', 'û').replace('Ã¢', 'â').replace('Ã¨', 'è').replace('Ã ', 'à').replace('â€™', "'").replace('â‚¬', '€').replace('Â®', '®').replace('Ã˜', 'Ø').replace('Â°', '°').replace('Ã§', 'ç').replace('Ã´', 'ô').replace('Â«', '«').replace('Â»', '»').replace('Ã»', 'û').replace('Ãª', 'ê').replace('â€¦', 'ø').replace('Ã€', 'A').replace('Ã‰', 'E')
 
-    # Replace each character with its corresponding replacement
-    for i in range(len(to_replace)):
-        text = text.replace(to_replace[i], replacements[i])
-    return text
+if len(sys.argv) < 3:
+    print("Usage: python script.py input_file output_file")
+    sys.exit(1)
 
-# Open the input and output files using context managers
-with open('_test.csv', 'r', encoding='cp1252') as infile, \
-     open('output.csv', 'w', encoding='cp1252', newline='') as outfile:
-    # Create CSV reader and writer objects
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(input_file, 'r', encoding='cp1252') as infile, \
+     open(output_file, 'w', encoding='cp1252', newline='') as outfile:
     reader = csv.reader(infile)
     writer = csv.writer(outfile)
-
-    # Iterate over each row in the input file
     for row in reader:
-        # Replace encoding errors in each cell of the row
         new_row = [fix_encoding(cell) for cell in row]
-        # Write the corrected row to the output file
         writer.writerow(new_row)
 
+print(f"Converted {input_file} to {output_file}")
